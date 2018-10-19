19b1aadc82380a27a5f296afe7a2ffc05eb615bf
Craig Lymus <craiglymus@gmail.com>
2018-10-19 14:20:40 -0700

    completed

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/craiglymus/WDI-48/HOMEWORK/WEEK-6/cat-collectr/.env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ main_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Cat
+
+class CatForm(forms.ModelForm):
+    class Meta:
+        model = Cat
+        fields = ('name', 'breed', 'description', 'age')
+    # name = forms.CharField(label='Name', max_length=100)
+    # breed = forms.CharField(label='Breed', max_length=100)
+    # description = forms.CharField(label='Description', max_length=250)
+    # age = forms.IntegerField(label='Age')
+class LoginForm(forms.Form):
+    username = forms.CharField(label="User Name", max_length=64)
+    password = forms.CharField(widget=forms.PasswordInput())
